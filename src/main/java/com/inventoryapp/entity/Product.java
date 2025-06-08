fbf4e7e42be2f33b0a4fcba490dba126cad22e0b
Ovett Mora <ovettmora17@gmail.com>
2025-06-08 13:46:59 -0600

    Add Product entity to represent products in the inventory

---
+++ src/main/java/com/inventoryapp/entity/Product.java
@@ -0,0 +1,38 @@
+package com.inventoryapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int stock;
+
+    private String category;
+}
